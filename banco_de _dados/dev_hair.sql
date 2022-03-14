f2a4cc4ff08ea848f472576da264d673352fda9e
Leticia <diogoleticia034@gmail.com>
2022-03-14 17:03:11 -0300

    feat: Alteração do banco, adicionando novas tabelas.

--- banco_de _dados/dev_hair.sql
+++ banco_de _dados/dev_hair.sql
@@ -2,35 +2,44 @@ create database dv_hstyle;
 
 use dv_hstyle;
 
-create table cliente(
-id int primary key auto_increment,
-cpf varchar(15)  not null,
-nome varchar(50) not null,
-dataNasc date default null,
-endereco varchar(50) not null,
-celular varchar(15) default null
+CREATE TABLE cliente(
+	id INT PRIMARY KEY auto_increment,
+    nome VARCHAR(255) NOT NULL,
+	telefone VARCHAR(15) DEFAULT NULL,
+	celular VARCHAR(15) DEFAULT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    dataCadastro DATE DEFAULT NULL
 );
 
-create table servico (
-cod int(11) primary key not null auto_increment,
-nome varchar(50) not null,
-descricao varchar (255) not null
+CREATE TABLE funcaofuncionario (
+id INT(11) PRIMARY KEY auto_increment,
+funcao VARCHAR(150) DEFAULT NULL
 );
 
-create table funcionario (
-id int(11) primary key not null auto_increment,
-nome varchar(50) not null,
-endereco varchar(50) not null,
-datanasc date not null,
-dataadm date not null
- 
+CREATE TABLE funcionario (
+	id INT(11) PRIMARY KEY auto_increment,
+	nome VARCHAR(255) DEFAULT NULL,
+	tipofun INT(11) NOT NULL,
+	FOREIGN KEY (tipofun) REFERENCES tipofuncionario(id)
 
+);
 
+CREATE TABLE servico (
+	id INT(11) PRIMARY KEY auto_increment,
+	nome VARCHAR(30) NOT NULL,
+	descricao VARCHAR(30) NOT NULL,
+	valordoser DECIMAL DEFAULT NULL
+);
 
-
-
-
-
+CREATE TABLE vendaservico(
+    id INT(11) PRIMARY KEY auto_increment,
+    idcliente INT(11) NOT NULL,
+    idfuncionario INT(11) NOT NULL,
+    idservico INT(11) NOT NULL,
+    Foreign key (idcliente) references  cliente(id),
+    Foreign key (idfuncionario) references funcionario(id),
+	Foreign key (idservico) references servico(id)
+);
 
 
 

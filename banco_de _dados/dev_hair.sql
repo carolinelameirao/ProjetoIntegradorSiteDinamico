898be73f18e727ccdc6ba1608ae24ef1eae472a4
Caroline Lameirão <carolinelameirao@outlook.com>
2022-03-14 21:54:12 -0300

    feat: Inclusão espaco.html, alterações dev_hair.sql, estilo.css, login.css, index.html, login.html

--- banco_de _dados/dev_hair.sql
+++ banco_de _dados/dev_hair.sql
@@ -2,43 +2,48 @@ create database dv_hstyle;
 
 use dv_hstyle;
 
-CREATE TABLE cliente(
-	id INT PRIMARY KEY auto_increment,
-    nome VARCHAR(255) NOT NULL,
-	telefone VARCHAR(15) DEFAULT NULL,
-	celular VARCHAR(15) DEFAULT NULL,
-    endereco VARCHAR(255) NOT NULL,
-    dataCadastro DATE DEFAULT NULL
+CREATE TABLE login (
+	email VARCHAR(150) PRIMARY KEY auto_increment,
+    senha VARCHAR(8) NOT NULL
 );
 
-CREATE TABLE funcaofuncionario (
-id INT(11) PRIMARY KEY auto_increment,
-funcao VARCHAR(150) DEFAULT NULL
+CREATE TABLE cliente (
+	cpf VARCHAR(11) PRIMARY KEY auto_increment,
+    nome VARCHAR(150) NOT NULL,
+	telefone VARCHAR(15) DEFAULT NULL,
+	celular VARCHAR(15) NOT NULL,
+    emailLogin VARCHAR(150) NOT NULL,
+    dataCadastro DATE DEFAULT NULL CURRENT_DATE,
+    FOREIGN KEY (emailLogin) REFERENCES login(email)
+);
+
+CREATE TABLE funcaoFuncionario (
+	id INT(11) PRIMARY KEY auto_increment,
+	funcao VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE funcionario (
 	id INT(11) PRIMARY KEY auto_increment,
-	nome VARCHAR(255) DEFAULT NULL,
-	tipofun INT(11) NOT NULL,
-	FOREIGN KEY (tipofun) REFERENCES tipofuncionario(id)
-
+	nome VARCHAR(150) NOT NULL,
+	idFuncaoFuncinario INT(11) NOT NULL,
+	FOREIGN KEY (idFuncaoFunconario) REFERENCES tipoFuncionario(id)
 );
 
 CREATE TABLE servico (
 	id INT(11) PRIMARY KEY auto_increment,
 	nome VARCHAR(30) NOT NULL,
 	descricao VARCHAR(30) NOT NULL,
-	valordoser DECIMAL DEFAULT NULL
+	valorServico DECIMAL NOT NULL
 );
 
-CREATE TABLE vendaservico(
+CREATE TABLE vendaServico (
     id INT(11) PRIMARY KEY auto_increment,
-    idcliente INT(11) NOT NULL,
-    idfuncionario INT(11) NOT NULL,
-    idservico INT(11) NOT NULL,
-    Foreign key (idcliente) references  cliente(id),
-    Foreign key (idfuncionario) references funcionario(id),
-	Foreign key (idservico) references servico(id)
+    idCliente INT(11) NOT NULL,
+    idFuncionario INT(11) NOT NULL,
+    idServico INT(11) NOT NULL,
+    Foreign key (idCliente) references cliente(id),
+    Foreign key (idFuncionario) references funcionario(id),
+	Foreign key (idServico) references servico(id)
 );
 
 

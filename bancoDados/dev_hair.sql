807cd5b6d8c3af59fcab9d34abc0d2e82d503d6f
Caroline Lameirão <carolinelameirao@outlook.com>
2022-03-16 20:48:49 -0300

    fix: correção da database

--- bancoDados/dev_hair.sql
+++ bancoDados/dev_hair.sql
@@ -1,49 +1,57 @@
+drop database dv_hstyle;
+
 create database dv_hstyle;
 
 use dv_hstyle;
 
 CREATE TABLE login (
-	email VARCHAR(150) PRIMARY KEY auto_increment,
+	id INT(11) PRIMARY KEY auto_increment,
+	email VARCHAR(150) NOT NULL,
     senha VARCHAR(8) NOT NULL
 );
 
 CREATE TABLE cliente (
-	cpf VARCHAR(11) PRIMARY KEY auto_increment,
+	cpf VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
 	telefone VARCHAR(15) DEFAULT NULL,
-	celular VARCHAR(15) NOT NULL,
-    emailLogin VARCHAR(150) NOT NULL,
-    dataCadastro DATE DEFAULT NULL CURRENT_DATE,
-    FOREIGN KEY (emailLogin) REFERENCES login(email)
+    idLogin INT(11) NOT NULL,
+    dataCadastro DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (idLogin) REFERENCES login(id)
 );
 
-CREATE TABLE funcaoFuncionario (
+CREATE TABLE funcao (
 	id INT(11) PRIMARY KEY auto_increment,
-	funcao VARCHAR(150) NOT NULL
+	atividade VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE funcionario (
 	id INT(11) PRIMARY KEY auto_increment,
 	nome VARCHAR(150) NOT NULL,
-	idFuncaoFuncinario INT(11) NOT NULL,
-	FOREIGN KEY (idFuncaoFunconario) REFERENCES funcaoFuncionario(id)
+	idFuncao INT(11) NOT NULL,
+	FOREIGN KEY (idFuncao) REFERENCES funcao(id)
 );
 
-CREATE TABLE servico (
+CREATE TABLE servicos (
 	id INT(11) PRIMARY KEY auto_increment,
 	nome VARCHAR(30) NOT NULL,
 	descricao VARCHAR(30) NOT NULL,
-	valorServico DECIMAL NOT NULL
+	valor DECIMAL(9,2) NOT NULL
 );
 
-CREATE TABLE vendaServico (
+CREATE TABLE horario (
+	id INT(11) PRIMARY KEY auto_increment,
+    hora TIME NOT NULL,
+    idServicos INT(11) NOT NULL,
+    FOREIGN KEY (idServicos) REFERENCES servicos(id)
+);
+
+CREATE TABLE agendamento (
     id INT(11) PRIMARY KEY auto_increment,
-    idCliente INT(11) NOT NULL,
-    idFuncionario INT(11) NOT NULL,
-    idServico INT(11) NOT NULL,
-    Foreign key (idCliente) references cliente(id),
-    Foreign key (idFuncionario) references funcionario(id),
-	Foreign key (idServico) references servico(id)
+    data DATE NOT NULL,
+    cpfCliente VARCHAR(11) NOT NULL,
+    idHorario INT(11) NOT NULL,
+    Foreign key (cpfCliente) references cliente(cpf),
+    Foreign key (idHorario) references horario(id)
 );
 
 

7251baff404bdf6e633eeb23cb2fc27d02a1fcbe
kmitiko <kmitiko1@gmail.com>
2022-03-17 22:54:44 -0300

    Feat: Inclusão de novas fotos

--- bancoDados/dev_hair.sql
+++ bancoDados/dev_hair.sql
@@ -7,6 +7,7 @@ CREATE TABLE login (
 	id INT(11) PRIMARY KEY auto_increment,
 	email VARCHAR(150) NOT NULL,
     senha VARCHAR(8) NOT NULL
+    
 );
 select * from  login;
 
@@ -18,6 +19,7 @@ CREATE TABLE cliente (
     dataCadastro DATE DEFAULT CURRENT_DATE,
     FOREIGN KEY (idLogin) REFERENCES login(id)
 );
+select * from cliente;
 
 CREATE TABLE funcao (
 	id INT(11) PRIMARY KEY auto_increment,
@@ -36,3 +38,18 @@ CREATE TABLE servicos (
 	nome VARCHAR(30) NOT NULL,
 	descricao VARCHAR(30) NOT NULL
 );
+
+CREATE OR REPLACE VIEW cliente_data as 
+SELECT 	c.idLogin ,
+		c.nome Nome,
+		c.cpf CPF, 
+		c.telefone Telefone,
+		c.dataCadastro DataCadastro,
+        l.email Email
+FROM cliente c 
+JOIN login l
+ON c.idLogin = l.id ;
+
+SELECT * FROM cliente_data;
+
+        
